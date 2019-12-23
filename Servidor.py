86ccf4b84cbff1994daf03a9605357595ef2e169
Nerd <46795193+NerdScript@users.noreply.github.com>
2019-12-23 13:14:04 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -1,3 +1,5 @@
+import socket
+
 ip = "" #127.0.0.1
 port = 443
 
@@ -5,6 +7,9 @@ s = socket.socket(AF_INET, SOCK_STREAM)
 
 s.bind((ip, port))
 s.listen(1)
-
 while True:
-    
+    c, e = s.accept()
+    print("Conectado com ", e)
+    msg = str(input(":> "))
+    c.send(msg.encode('ascii'))
+    c.close()
